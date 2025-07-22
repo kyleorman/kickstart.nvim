bf20b6aee146e49b181b67c1a7296085875e9111
Kyle Orman <ko@wificampus-036039.grenet.fr>
2025-07-22 14:33:34 +0200

    Fixed cmp mapping

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -24,36 +24,33 @@ return {
       }),
 
       -- Define all keymaps EXCEPT the Enter key here
-      mapping = {
-        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
-        ['<C-j>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
-        ['<C-Space>'] = cmp.mapping.complete(),
+	mapping = {
+	  ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+	  ['<C-j>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+	  ['<C-Space>'] = cmp.mapping.complete(),
+	  ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          -- ✅ THIS IS THE FIX: Check that fallback is a function before calling it
-          elseif type(fallback) == 'function' then
-            fallback()
-          end
-        end, { 'i', 's' }),
+	  ['<Tab>'] = cmp.mapping(function(fallback)
+	    if cmp.visible() then
+	      cmp.select_next_item()
+	    elseif luasnip.expand_or_jumpable() then
+	      luasnip.expand_or_jump()
+	    else
+	      fallback()
+	    end
+	  end, { 'i', 's' }),
 
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          -- ✅ THIS IS THE FIX: Check that fallback is a function before calling it
-          elseif type(fallback) == 'function' then
-            fallback()
-          end
-        end, { 'i', 's' }),
-      },
+	  ['<S-Tab>'] = cmp.mapping(function(fallback)
+	    if cmp.visible() then
+	      cmp.select_prev_item()
+	    elseif luasnip.jumpable(-1) then
+	      luasnip.jump(-1)
+	    else
+	      fallback()
+	    end
+	  end, { 'i', 's' }),
+	},
     }
 
-    -- Force the Enter key mapping using Neovim's core API
-    vim.keymap.set('i', '<CR>', cmp.mapping.confirm { select = true })
   end,
 }
