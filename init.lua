43ec3fe9f7ab245a803bea90a94e399fee04b3e1
kyleorman <kaorman@uark.edu>
2026-02-06 15:24:42 -0600

    Fix nvim-treesitter module name: config -> configs
    The 'main' field pointed to 'nvim-treesitter.config' which does not exist.
    The correct module is 'nvim-treesitter.configs'.

--- init.lua
+++ init.lua
@@ -942,7 +942,7 @@ require('lazy').setup({
 { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.config', -- NEW: module name changed
+  main = 'nvim-treesitter.configs',
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = {
